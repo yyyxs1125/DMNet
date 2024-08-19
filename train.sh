339f17a972c45594c6e67d6c9bb024d5341c08d3
Yixin Sun <lmfm45@ncc1.clients.dur.ac.uk>
2024-08-19 11:49:32 +0100

    test the branch

--- train.sh
+++ train.sh
@@ -13,5 +13,5 @@ eval "$(conda shell.bash hook)"
 conda activate openmmlab
 
 module load cuda/11.8
-# python tools/train.py /home2/lmfm45/mmsegmentation/configs/dmnet/dmnet_r50-d8_512x1024_40k_mydata.py
-python /home2/lmfm45/mmsegmentation/mmseg/utils/mean_std.py
+python tools/train.py /home2/lmfm45/mmsegmentation/configs/dmnet/dmnet_r50-d8_512x1024_40k_mydata.py
+# python /home2/lmfm45/mmsegmentation/mmseg/utils/mean_std.py
