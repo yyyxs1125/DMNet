6627417536047961c100abf69e95130fb4ecb81d
Yixin Sun <lmfm45@ncc1.clients.dur.ac.uk>
2024-08-19 11:47:17 +0100

    change the bacthsize and mean std

--- configs/_base_/datasets/my_dataset.py
+++ configs/_base_/datasets/my_dataset.py
@@ -1,8 +1,9 @@
 dataset_type = 'MyDataset'
 data_root = 'data/my_dataset'
 
+# mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375]
 img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+     mean=[85.6545, 96.237, 73.3335], std=[43.942, 44.778, 44.829], to_rgb=True)
 crop_size = (640, 400)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
@@ -26,7 +27,7 @@ test_pipeline = [
 ]
 
 train_dataloader = dict(  # 训练数据加载器(dataloader)的配置
-    batch_size=16,  # 每一个GPU的batch size大小
+    batch_size=28,  # 每一个GPU的batch size大小
     num_workers=8,  # 为每一个GPU预读取数据的进程个数
     persistent_workers=True,  # 在一个epoch结束后关闭worker进程，可以加快训练速度
     sampler=dict(type='InfiniteSampler', shuffle=True),  # 训练时进行随机洗牌(shuffle)
